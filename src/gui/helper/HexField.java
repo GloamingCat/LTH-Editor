57a19fd931f4cff0fefadac6673899d37cc08ffe
GloamingCat <nightlywhiskers@gmail.com>
2023-10-11 21:30:34 -0300

    HexH autotiles

--- src/gui/helper/HexField.java
+++ src/gui/helper/HexField.java
@@ -33,33 +33,69 @@ public class HexField extends BaseIsoField {
 	public int[] autotile(int[][] grid, int x, int y) {
 		int[] rows = new int[] {0, 0, 0, 0};
 		Point p = new Point(x, y);
-		// TODO: horizontally connected
-		if (sameType(grid, p, 1, -1)) {
-			rows[0] = rows[0] + 1;
-			rows[1] = rows[1] + 1;		  
-		}
-		if (sameType(grid, p, -1, 1)) {
-			rows[2] = rows[2] + 1;
-			rows[3] = rows[3] + 1;		  
+		
+		if (conf.tileS == 0) {
+			// Vertically connected
+			if (sameType(grid, p, 1, -1)) {
+				rows[0] = rows[0] + 1;
+				rows[1] = rows[1] + 1;		  
+			}
+			if (sameType(grid, p, -1, 1)) {
+				rows[2] = rows[2] + 1;
+				rows[3] = rows[3] + 1;		  
+			}
+			
+			if (sameType(grid, p, 1, 0)) {
+				rows[1] = rows[1] + 2;
+				rows[3] = rows[3] + 2;		  
+			}
+			if (sameType(grid, p, -1, 0)) {
+				rows[0] = rows[0] + 2;
+				rows[2] = rows[2] + 2;
+			}
+			
+			if (sameType(grid, p, 0, 1)) {
+				rows[1] = rows[1] + 4;
+				rows[3] = rows[3] + 4;		  
+			}
+			if (sameType(grid, p, 0, -1)) {
+				rows[0] = rows[0] + 4;
+				rows[2] = rows[2] + 4;		  
+			}
+		
 		}
 		
-		if (sameType(grid, p, 1, 0)) {
-			rows[1] = rows[1] + 2;
-			rows[3] = rows[3] + 2;		  
-		}
-		if (sameType(grid, p, -1, 0)) {
-			rows[0] = rows[0] + 2;
-			rows[2] = rows[2] + 2;		  
+		if (conf.tileB == 0) {
+			// Horizontally connected
+			if (sameType(grid, p, -1, -1)) {
+				rows[0] += 1;
+				rows[2] += 1;		  
+			}
+			if (sameType(grid, p, 1, 1)) {
+				rows[1] += 1;
+				rows[3] += 1;		  
+			}
+		
+			if (sameType(grid, p, 1, 0)) {
+				rows[1] += 2;
+				rows[0] += 2;		  
+			}
+			if (sameType(grid, p, -1, 0)) {
+				rows[3] += 2;
+				rows[2] += 2;
+			}
+			
+			if (sameType(grid, p, 0, 1)) {
+				rows[3] += 4;
+				rows[2] += 4;		  
+			}
+			if (sameType(grid, p, 0, -1)) {
+				rows[1] += 4;
+				rows[0] += 4;
+			}
+
 		}
 		
-		if (sameType(grid, p, 0, 1)) {
-			rows[1] = rows[1] + 4;
-			rows[3] = rows[3] + 4;		  
-		}
-		if (sameType(grid, p, 0, -1)) {
-			rows[0] = rows[0] + 4;
-			rows[2] = rows[2] + 4;		  
-		}
 		if (rows[0] == 0 && rows[1] == 0 && rows[2] == 0 && rows[3] == 0) {
 			rows[0] = rows[1] = rows[2] = rows[3] = 8;
 		}
